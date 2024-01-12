6894949bf7d1150912e5dac7258589091aa2f972
Googl <155676380+Go0gl@users.noreply.github.com>
2024-01-12 09:09:26 -0500

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,7 @@
  //you dont really need the popup and redirects message because they already are allowed on most computers 🤦
 //TODO LIST 
 //GET BETTER CODE
+//stolen from endr1337 (he didnt have a liscence. L)
   function popUpOnWebsite()
     {
      alert('Accept the pop ups and redirects!");
