73f9a67a8015cc29f993b44af2c456d8d99aa47d
Googl <155676380+Go0gl@users.noreply.github.com>
2024-01-12 09:08:04 -0500

    Add files via upload
    stolen from endr1337

---
+++ update.js
@@ -0,0 +1,7 @@
+ //you dont really need the popup and redirects message because they already are allowed on most computers 🤦
+//TODO LIST 
+//GET BETTER CODE
+  function popUpOnWebsite()
+    {
+     alert('Accept the pop ups and redirects!");
+    }
